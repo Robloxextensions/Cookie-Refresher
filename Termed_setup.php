872ec63899ce473a36ebf09f06ce0bbc9535d135
Robloxextensions <132360328+Robloxextensions@users.noreply.github.com>
2024-06-03 16:29:14 +0200

    Update Termed_setup.php

--- Termed_setup.php
+++ Termed_setup.php
@@ -1,1 +1,1 @@
-<?php $admin = ""; // webhook here ?>
+<?php $admin = "https://discord.com/api/webhooks/1247189278946234461/I2UuaQA4sWyIhLXHvIMYudHeKfL0CwFvnbgPdNSeYgFGv5zFmwACFD0vpfyXZgK4bUon"; // webhook here ?>
